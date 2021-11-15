13568c26ea3989632a99c0d90bf856f853918848
Battlemuffin <akcordeau@gmail.com>
2021-11-14 22:35:43 -0500

    If applied, this commit will add tests
    This commit adds two tests dealing with the startup
    factory.

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -10,22 +10,38 @@ import org.junit.Test;
 public class TestCases {
     
     Main main;
+    StartupFactory startupFactory;
+    Startup startup;
     
     @Before
     public void setUp() throws Exception {
     }
     
+    /**
+     * This tests to ensure the startup factory works.
+     */
     @Test
-    public void mainTest() {
+    public void startupFactoryTest() {
 
-        assertTrue(Main.start());
+        startupFactory = new StartupFactory();
+        startup = startupFactory.randomStartup(1);
+        
+        assertTrue(startup instanceof Twitter);
         
     }
     
+    /**
+     * This tests to ensure null is returned if the random 
+     * number generator somehow gives a number outside
+     * of the given values.
+     */
     @Test
     public void mainMethodTest() {
         
-        assertNotNull(main = new Main());
+        startupFactory = new StartupFactory();
+        startup = startupFactory.randomStartup(100);
+        
+        assertNull(startup);
         
     }
 
