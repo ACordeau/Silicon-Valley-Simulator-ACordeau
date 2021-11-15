5dcaff90bea4bb2f0891b698a27e516e3fade4bd
Battlemuffin <akcordeau@gmail.com>
2021-11-15 17:48:02 -0500

    If applied, this commit will update Main
    This commit is the beginning of the gameplay loop
    where everything will take place.

--- src/main/java/svalley/Main.java
+++ src/main/java/svalley/Main.java
@@ -1,14 +1,43 @@
 package main.java.svalley;
 
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Random;
+
 public class Main {
-    
+
     public static void main(String[] args) {
         start();
     }
-    
+
     public static boolean start() {
-        System.out.println("Hello World!");
+        // System.out.println("Hello World!");
+
+        int random;
+        TechGiantFactory tgf = new TechGiantFactory();
+        
+        TechGiant tg1 = null;
+        random = randomTechGiantNumber();
+        tg1 = tgf.randomTechGiant(random);
+        
+        TechGiant tg2 = null;
+        do {
+            random = randomTechGiantNumber();
+            tg2 = tgf.randomTechGiant(random);
+        } while (tg1.getName().equals(tg2.getName()));
+        
+        tg1.initialStartup();
+        tg2.initialStartup();
+
         return true;
+    }
+    
+    public static int randomTechGiantNumber() {
+        
+        Random rand = new Random();
+        int random;
+        random = rand.nextInt(4 - 1 + 1) + 1;
+        return random;
     }
 
 }
